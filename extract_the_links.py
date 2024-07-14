08b12ae4dc774552ca0d5c1fa56a678c3e923734
YanaZhekova <ianagekowa@gmail.com>
2024-07-14 11:56:33 +0300

    Lecture 9 exercises

--- extract_the_links.py
+++ extract_the_links.py
@@ -2,7 +2,7 @@ import re
 
 text = input()
 while text != "":
-    matches = re.finditer(r"(?P<sub_domain>w{3})[.]{1}(?P<domain>[a-zA-Z]+[0-9]*([-]?)[a-zA-Z]*[0-9]*\3?[a-zA-Z]+[0-9]*)[.]{1}(?P<domain_extension>[.]*[a-zA-Z]+)+",text)
+    matches = re.finditer(r"(?P<sub_domain>w{3})[.]{1}(?P<domain>[0-9]*[a-zA-Z]*[0-9]*([-]?)[a-zA-Z]*[0-9]*\3?[a-zA-Z]*[0-9]*)[.]{1}(?P<domain_extension>[a-zA-Z]*[.]*[a-zA-Z]+)+",text)
     for match in matches:
         print(match.group())
     text = input()
